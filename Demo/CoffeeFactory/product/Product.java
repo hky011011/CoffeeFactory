11b28fbb8d2384f973fdcab25a3447f5e26740b1
Tianyu Lou <3089608641@qq.com>
2021-10-12 16:22:07 +0800

    Finished abstract Product

--- Demo/CoffeeFactory/product/Product.java
+++ Demo/CoffeeFactory/product/Product.java
@@ -1,5 +1,6 @@
 package Demo.CoffeeFactory.product;
 
+import Demo.CoffeeFactory.product.production.Packed;
 import Demo.CoffeeFactory.product.production.Producing;
 import Demo.CoffeeFactory.product.production.ProductionStage;
 
@@ -9,7 +10,7 @@ public abstract class Product {
     private String productName;
     private String productionDate;
     private int shelfLife;
-    private float weight;
+    private int weight;
     public ProcessMachine produceMachine, wrapperMachine;
     protected ProductionStage stage;
     protected PackageMaterial pack;
@@ -40,6 +41,64 @@ public abstract class Product {
     public void setStage(ProductionStage stage) {
         this.stage = stage;
     }
+    public void nextStage() {
+        stage.nextState(this);
+    }
+
+    public void setProductionDate(String date) {
+        if (this.productionDate == null) {
+            this.productionDate = date;
+        }
+        else {
+            System.out.println("Warning! The production date CAN NOT be modified!");
+        }
+    }
+    public String getProductionDate() {
+        return this.productionDate;
+    }
+
+    protected void setShelfLife(int shelfLife) {
+        this.shelfLife = shelfLife;
+    }
+    public int getShelfLife() {
+        return this.shelfLife;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setPackage(PackageMaterial pack) {this.pack = pack;}
+
+    public void setWrapperMachine(ProcessMachine wrapperMachine) {
+        this.wrapperMachine = wrapperMachine;
+    }
+    public void setProduceMachine(ProcessMachine produceMachine) {
+        this.produceMachine = produceMachine;
+    }
+
+    public void produce() {
+        this.produceMachine.process(this, 1);
+    }
+    public void pack() {
+        this.wrapperMachine.process(this, 1);
+    }
+    public void store() {
+        if (!(this.stage instanceof Packed)) {
+            System.out.println("The product has not been packaged!");
+            return;
+        }
+        System.out.println("Transporting " + getProductName() + " to the warehouse.");
+        this.nextStage();
+    }
+
+
+
+
+
 
 
 
